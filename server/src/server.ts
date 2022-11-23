76af80c570859b4908af97568a7978069b8e475c
abhayc-glitch <78511893+abhayc-glitch@users.noreply.github.com>
2022-11-22 18:25:11 -0800

    wrote connection code to mongodb

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,3 +5,22 @@ import cors from 'cors';
 import { ConnectOptions } from 'mongoose';
 
 
+const app = express();
+
+// BodyParser methods - limit to be 30mb
+app.use(bodyParser.json({limit: "30mb"}));
+// urlencoded strings only
+app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
+// CORS - cross origin request - authorizes requests from a different server
+// Once the "access-control-allow-origin" header is matched with the request origin header then data can be passed through
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://abhay:Abhay1234@cluster0.4cpfcbs.mongodb.net/?retryWrites=true&w=majority'
+const port = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(port, () => {
+        console.log('server listening on port'+port)
+    }))
+    .catch((error)=> console.log(error));
+
